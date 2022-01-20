7888d82ac04d2004f92569ca7979aac4e5b3513e
ypodlesov <ypodlesov@gmail.com>
2022-01-20 23:07:53 +0300

    reg-2020-2021-A-B-C

--- olimpiads_review/region_2020-2021/day_1/B.cpp
+++ olimpiads_review/region_2020-2021/day_1/B.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 
 #define ll long long
+
 ll n, m;
 
-ll vert_sum(int j) {
-    return j * n * (1 + j + m * (n - 1)) / 2;
+ll vert_sum(ll j) {
+    return (n * j * (1 + j + (n - 1) * m)) / 2;
 }
 
-ll hor_sum(int i) {
-    return i * m * (1 + m + m * (i - 1)) / 2;
+ll hor_sum(ll i) {
+    return m * i * (1 + m + m * (i - 1)) / 2;
 }
 
 int main() {
@@ -27,7 +28,7 @@ int main() {
             l += step;
         }
         ll vert = l;
-        if (abs(2 * vert_sum(l) - sum) > abs(2 * vert_sum(l + 1) - sum)) ++vert;
+        if (abs(2 * vert_sum(vert + 1) - sum) < abs(2 * vert_sum(vert) - sum)) ++vert;
 
         // horizontal cut
         l = 0, step = n;
@@ -36,13 +37,12 @@ int main() {
             l += step;
         }
         ll hor = l;
-        if (abs(2 * hor_sum(l) - sum) > abs(2 * hor_sum(l + 1) - sum)) ++hor;
+        if (abs(2 * hor_sum(hor + 1) - sum) < abs(2 * hor_sum(hor) - sum)) ++hor;
 
         ll a = abs(2 * vert_sum(vert) - sum);
         ll b = abs(2 * hor_sum(hor) - sum);
         if (a <= b) cout << "V " << vert + 1 << endl;
         else cout << "H " << hor + 1 << endl;
-
     }
 
     return 0;
